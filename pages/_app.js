47d7649d31545298b53a153f5e6230ecaa1fe7a8
krakenyyc <carcln@me.com>
2021-01-31 19:51:44 -0700

    adding new pages components and models

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+import * as Realm from "realm-web";
+
+const REALM_APP_ID = "repoquicksilver-app"; // e.g. myapp-abcde
+const app = new Realm.App({ id: REALM_APP_ID });
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
